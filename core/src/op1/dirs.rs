5c2c5036c029cffd4c1f701102a64161e6c8311a
emannguitar <emannguitar@gmail.com>
2022-01-30 12:49:20 -0500

    Fixed op1 discovery

--- core/src/op1/dirs.rs
+++ core/src/op1/dirs.rs
@@ -60,22 +60,28 @@ impl TryFrom<PathBuf> for OP1Dirs {
     // TODO: Handle/log IO errors when checking if dir exists
     fn try_from(parent_dir: PathBuf) -> Result<Self, Self::Error> {
         let album = parent_dir.join("album");
-        album
-            .try_exists()
-            .map_err(|_| Error::MissingDir("album".to_owned()))?;
+        match album.try_exists() {
+            Ok(true) => (),
+            _ => return Err(Error::MissingDir("album".to_owned())),
+        }
 
         let drum = parent_dir.join("drum");
-        drum.try_exists()
-            .map_err(|_| Error::MissingDir("drum".to_owned()))?;
+        match drum.try_exists() {
+            Ok(true) => (),
+            _ => return Err(Error::MissingDir("drum".to_owned())),
+        }
 
         let synth = parent_dir.join("synth");
-        synth
-            .try_exists()
-            .map_err(|_| Error::MissingDir("synth".to_owned()))?;
+        match synth.try_exists() {
+            Ok(true) => (),
+            _ => return Err(Error::MissingDir("synth".to_owned())),
+        }
 
         let tape = parent_dir.join("tape");
-        tape.try_exists()
-            .map_err(|_| Error::MissingDir("tape".to_owned()))?;
+        match tape.try_exists() {
+            Ok(true) => (),
+            _ => return Err(Error::MissingDir("tape".to_owned())),
+        }
 
         Ok(OP1Dirs {
             parent_dir,
